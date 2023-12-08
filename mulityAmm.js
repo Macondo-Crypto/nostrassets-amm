60a26b82d28fecb367f48f4bd612cb1b264eaee5
Your Name <you@example.com>
2023-12-08 15:13:51 +0800

    make request in promise

---
+++ mulityAmm.js
@@ -0,0 +1,48 @@
+const api = require("./utils/api");
+
+
+function sleep (ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+}
+
+async function test()
+{
+    const a = await api.promiseNostrassets();
+    console.log(a)
+}
+
+async function dataMatch(BUYFROM,SELLTO,TOKEN)
+{
+    var subPriceAlam = BUYFROM[0].price * 0.01
+    if(SELLTO[0].price-BUYFROM[0].price > subPriceAlam)
+    {
+        console.log(`🍺 ${TOKEN} Avable for amm :: BUYFROM :: ${BUYFROM[0].id} : ${BUYFROM[0].price}` , ` | SELL TO :: ${SELLTO[0].id} : ${SELLTO[0].price}`)
+        //Check order type 
+    }
+}
+
+async function loopAmm()
+{
+    const data = await api.promiseNostrassets();
+    var TRICKSELL = data[0];
+    var TRICKBUY = data[1];
+    var TREATSELL = data[2];
+    var TREATBUY = data[3];
+
+    await dataMatch(TRICKSELL,TRICKBUY,"TRICK")
+    await dataMatch(TREATSELL,TREATBUY,"TREAT")
+}
+
+async function init()
+{
+    while(true)
+    {
+        await loopAmm();
+        await sleep(3000)
+    }
+}
+
+// test()
+init()
--- utils/api.js
+++ utils/api.js
@@ -12,7 +12,7 @@ async function nostrassetsGraph (token,type,order_by)
             {
                 "query": `query ($offset: Int!, $limit: Int!) {\n  nostr_market_order(\n    limit: $limit\n    offset: $offset\n    where: {status: {_in: [\n\"INIT\", \n\"PUSH_MARKET_SUCCESS\", \n\"PART_SUCCESS\"]}, type: {_eq: \n\"${type}\"}, token: {_eq: \n\"${token}\"}}\n    order_by: {price: ${order_by}}\n  ) {\n    avg_price\n    create_time\n    deal_money\n    deal_volume\n    event_id\n    id\n    modify_time\n    owner\n    price\n    status\n    token\n    token_address\n    total_price\n    trade_fee\n    type\n    volume\n    __typename\n  }\n  nostr_market_order_aggregate(\n    where: {status: {_in: [\n\"INIT\", \n\"PUSH_MARKET_SUCCESS\", \n\"PART_SUCCESS\"]}, type: {_eq: \n\"SELL\"}, token: {_eq: \n\"TREAT\"}}\n  ) {\n    aggregate {\n      count\n      __typename\n    }\n    __typename\n  }\n}`,
                 "variables": {
-                  "limit": 1,
+                  "limit": 3,
                   "offset": 0
                 }
               }
@@ -21,6 +21,41 @@ async function nostrassetsGraph (token,type,order_by)
       return req.doRequest(options);
 }
 
+async function promiseNostrassets()
+{
+  var data = [
+    {
+      name:"TRICK",
+      type:"SELL",
+      order_by:"asc"
+    },
+    {
+      name:"TRICK",
+      type:"BUY",
+      order_by:"desc"
+    },
+    {
+      name:"TREAT",
+      type:"SELL",
+      order_by:"asc"
+    },
+    {
+      name:"TREAT",
+      type:"BUY",
+      order_by:"desc"
+    }
+  ]
+
+  const contractRequest = data.map((d) => {
+    return nostrassetsGraph(d.name,d.type,d.order_by)
+   .then((a) => {
+    return a.data.nostr_market_order
+    })
+})
+return Promise.all(contractRequest)
+}
+
 module.exports = {
     nostrassetsGraph,
+    promiseNostrassets
 }
